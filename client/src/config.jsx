7adcafee03e772e89469243660a56d22511f614e
naneen <nazneenbanosiddiqui@gmail.com>
2025-03-22 14:11:14 +0530

    open

--- client/src/config.jsx
+++ client/src/config.jsx
@@ -1,4 +1,4 @@
-//const BASE_URL="http://localhost:8010";
+// const BASE_URL="http://localhost:8010";
  const BASE_URL="https://studentteachercurdopretionmern.onrender.com";
 
 export default BASE_URL;
